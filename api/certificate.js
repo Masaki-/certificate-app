dc441d17780246ec0759b36b95038a794dc9f6f9
柴田　将希 <Masaki_tele@yahoo.co.jp>
2025-06-10 10:53:31 +0900

    fix :

--- api/certificate.js
+++ api/certificate.js
@@ -1,13 +1,39 @@
 const puppeteer = require("puppeteer-core");
 const chrome = require("chrome-aws-lambda");
-const fs = require("fs");
-const path = require("path");
 
 module.exports = async (req, res) => {
   const { name = "受講者" } = req.query;
 
-  const templatePath = path.join(__dirname, "../templates/certificate.html");
-  const html = fs.readFileSync(templatePath, "utf8").replace("{{name}}", name);
+  const html = `
+    <html>
+      <head>
+        <meta charset="UTF-8">
+        <style>
+          body {
+            width: 1200px;
+            height: 800px;
+            margin: 0;
+            font-family: 'Noto Sans JP', sans-serif;
+            background: url('https://${
+              req.headers.host
+            }/assets/certificate-template.png') no-repeat center center;
+            background-size: cover;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
+          .name {
+            font-size: 48px;
+            color: #333;
+            text-align: center;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="name">${decodeURIComponent(name)}</div>
+      </body>
+    </html>
+  `;
 
   const browser = await puppeteer.launch({
     args: chrome.args,
@@ -16,10 +42,10 @@ module.exports = async (req, res) => {
   });
 
   const page = await browser.newPage();
+  await page.setViewport({ width: 1200, height: 800 });
   await page.setContent(html, { waitUntil: "networkidle0" });
 
-  const buffer = await page.screenshot({ type: "png", fullPage: true });
-
+  const buffer = await page.screenshot({ type: "png" });
   await browser.close();
 
   res.setHeader("Content-Type", "image/png");
