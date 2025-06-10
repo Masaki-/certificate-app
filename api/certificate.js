317b59a2dd83a7cdc0f4fce6ac2761325b508823
柴田　将希 <Masaki_tele@yahoo.co.jp>
2025-06-10 10:50:55 +0900

    fix : Puppeteer で修了証を描画 → PNG画像として返す

--- api/certificate.js
+++ api/certificate.js
@@ -1,30 +1,27 @@
-// api/certificate.js
-const { createCanvas, loadImage, registerFont } = require("canvas");
+const puppeteer = require("puppeteer-core");
+const chrome = require("chrome-aws-lambda");
+const fs = require("fs");
 const path = require("path");
-
-// 日本語フォントを使いたい場合は登録（Vercelにフォントファイルもアップロードする必要あり）
-// registerFont(path.join(__dirname, '../assets/NotoSansJP-Regular.otf'), { family: 'Noto Sans JP' });
 
 module.exports = async (req, res) => {
   const { name = "受講者" } = req.query;
 
-  const width = 1200;
-  const height = 800;
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext("2d");
+  const templatePath = path.join(__dirname, "../templates/certificate.html");
+  const html = fs.readFileSync(templatePath, "utf8").replace("{{name}}", name);
 
-  const templatePath = path.join(
-    __dirname,
-    "../assets/certificate-template.png"
-  );
-  const background = await loadImage(templatePath);
-  ctx.drawImage(background, 0, 0, width, height);
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
 
-  ctx.font = "bold 48px sans-serif";
-  ctx.fillStyle = "#333";
-  ctx.textAlign = "center";
-  ctx.fillText(decodeURIComponent(name), width / 2, height / 2 + 50);
+  const page = await browser.newPage();
+  await page.setContent(html, { waitUntil: "networkidle0" });
+
+  const buffer = await page.screenshot({ type: "png", fullPage: true });
+
+  await browser.close();
 
   res.setHeader("Content-Type", "image/png");
-  canvas.pngStream().pipe(res);
+  res.send(buffer);
 };
